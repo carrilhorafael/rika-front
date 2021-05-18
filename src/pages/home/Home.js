96643c2c7944cef2eb6398cfd39ce90086cc55cd
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-18 15:30:18 -0300

    add home basics config

---
+++ src/pages/home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Home () {
+    return(
+        <>
+            <h1>Essa é a home</h1>
+            <a href="/initial"></a>
+        </>
+    )
+}
