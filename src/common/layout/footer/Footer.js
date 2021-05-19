027a527c12f1752e550c2bdee0a951ab08b90b4b
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-19 16:57:45 -0300

    fix nav links

--- src/common/layout/footer/Footer.js
+++ src/common/layout/footer/Footer.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
 import style from '../layout.module.css'
+import lobo_facebook from '../../../assets/facebook.png'
 
 export default function(){
 
     return(
         <footer className={style.grid_footer_area}>
             <p>Desenvolvido por Rafael Carrilho</p>
+            <div>
+                <Link to="/">Facebook</Link>
+                <Link to="/">Instagram</Link>
+                <Link to="/">LinkedIN</Link>
+            </div>
         </footer>
     )
 }
