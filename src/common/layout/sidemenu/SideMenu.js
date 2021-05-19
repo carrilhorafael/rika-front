cb29db03e260d792fc8e88652fa15704090b6763
Carrilho <rafael.carrilho@injunior.com.br>
2021-05-19 11:23:28 -0300

    redo navbar with buttons

--- src/common/layout/sidemenu/SideMenu.js
+++ src/common/layout/sidemenu/SideMenu.js
@@ -3,11 +3,18 @@ import {Link} from 'react-router-dom'
 import style from "../layout.module.css"
 
 export default function SideMenu () {
+    const handlerToggleShow = (e) => {
+        e.target.nextSibling.style.display = e.target.nextSibling.style.display === "none" ? "flex" : "none" 
+    }
     return (<nav className={style.grid_nav_area}>
-        <Link to="/">Link 1</Link>
-        <Link to="/">Link 2</Link>
-        <Link to="/">Link 3</Link>
-        <Link to="/">Link 4</Link>
-        <Link to="/">Link 5</Link>
+        <Link to="/"><button className={style.button_nav_type}>Principal</button></Link>
+            <Link to="/schedule"><button className ={style.button_nav_type}>Aulas</button></Link>
+            <button onClick={handlerToggleShow} className = {style.button_nav_type}>Entregas</button>
+            <div style={{display:"none"}} className={style.div_buttons_nav_bar}>
+                <Link><button className={style.button_nav_type}>Tarefas</button></Link>
+                <Link><button className={style.button_nav_type}>Feedbacks</button></Link>
+                <Link><button className={style.button_nav_type}>Minhas entregas</button></Link>
+            </div>
+            <Link to="/logout"><button className = {style.button_nav_type}>Sair</button></Link>
     </nav>)
 }
